f64e5042b6be5b41395e6a64c3f475fe583686fe
Patric Vormstein <vormstein@med.uni-frankfurt.de>
2017-08-31 15:29:10 +0200

    Automatically copy config

--- instructions.js
+++ instructions.js
@@ -1,9 +1,20 @@
 'use strict'
 
+const path = require('path')
+
 async function copyConfig (cli) {
+  const configPath = path.join(cli.helpers.appRoot(), 'config')
   try {
-    console.log(`cli.configDir: ${cli.configDir}`)
+    await cli.copy(
+      path.join(__dirname, 'config', 'eureka.js'),
+      path.join(configPath, 'eureka.js')
+    )
+    cli.command.completed('Copy', 'config/eureka.js')
   } catch (error) {
     consoloe.log(error)
   }
+}
+
+module.exports = async (cli) => {
+  await copyConfig(cli)
 }
