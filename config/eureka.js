acf1fb0bce7e7603c099772582de47684f74083c
Patric Vormstein <vormstein@med.uni-frankfurt.de>
2017-08-31 16:42:01 +0200

    Config with comments

--- config/eureka.js
+++ config/eureka.js
@@ -1,14 +1,43 @@
 'use strict'
 
+/*
+|--------------------------------------------------------------------------
+| Services Configuration
+|--------------------------------------------------------------------------
+|
+| This is general purpose file to define configuration for multiple services.
+| The below config is for the eureka provider. Make sure to have it inside
+| config/eureka.js available.
+|
+*/
+
 const Env = use('Env')
 
 module.exports = {
+  /*
+  |--------------------------------------------------------------------------
+  | Server Configuration
+  |--------------------------------------------------------------------------
+  |
+  | This is the configuration for the server, which defines how to resolve it.
+  | If using Spring Cloud, you may use '/eureka/apps/' as servicePath.
+  |
+  */
   server: {
     host: Env.get('EUREKA_SERVER_HOST') || 'localhost',
     port: Env.get('EUREKA_SERVER_POST') || 8761,
     servicePath: Env.get('EUREKA_SERVER_SERVICE_PATH') || '/eureka/apps/'
   },
   
+  /*
+  |--------------------------------------------------------------------------
+  | Instance Configuration
+  |--------------------------------------------------------------------------
+  | 
+  | This configuration defines, how the client will be registered on the 
+  | eureka server.
+  |
+  */
   instance: {
     appName: Env.get('EUREKA_INSTANCE_APP_NAME') || 'AdonisJs Instance',
     hostname: Env.get('EUREKA_INSTANCE_HOSTNAME') || 'localhost',
